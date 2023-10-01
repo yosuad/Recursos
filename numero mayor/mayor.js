1d01bb39e638f3e9b2bb7b44791d25de4760b7c1
yosuad <yosuad@hotmail.com>
2023-10-01 09:37:24 -0500

    update_seven

---
+++ numero mayor/mayor.js
@@ -0,0 +1,9 @@
+var valor1 = parseInt(window.prompt("Ingrese un numero")); 
+var valor2 = parseInt(window.prompt("Ingrese otro numero")); 
+
+if(valor1 > valor2){
+    alert("Valor 1 es mayor");
+}
+else{
+    alert("Valor 2 es mayor");
+}
