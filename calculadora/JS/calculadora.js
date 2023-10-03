8e1bc72b7cb558a5726c48116cda757ac9be886f
yosuad <yosuad@hotmail.com>
2023-10-02 20:28:46 -0500

    update_nine

--- calculadora/JS/calculadora.js
+++ calculadora/JS/calculadora.js
@@ -1,4 +1,15 @@
 function mostrarResultado(resultado){
     document.getElementById("resultado").value = resultado;
+}
 
+function suma(){
+    let numero1 = +document.getElementById("campo1").value;
+    let numero2 = +document.getElementById("campo2").value;
+    mostrarResultado(numero1 + numero2)
+}
+
+function resta(){
+    let numero1 = +document.getElementById("campo1").value;
+    let numero2 = +document.getElementById("campo2").value;
+    mostrarResultado(numero1 - numero2)
 }
