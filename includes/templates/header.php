57cfdf4ae54b1b2d8ab4d083c65ca5805820acc8
yosuad <yosuad@hotmail.com>
2024-05-16 21:02:03 -0500

    bienes_raices_inicio

---
+++ includes/templates/header.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bienes Raices</title>
+    <link rel="stylesheet" href="build/css/app.css">
+</head>
+<body>
+    
+    <header class="header inicio">
+        <div class="contenedor contenido-header">
+            <div class="barra">
+                <a href="/">
+                    <img src="build/img/logo.svg" alt="Logotipo de Bienes Raices">
+                </a>
+
+                <div class="mobile-menu">
+                    <img src="build/img/barras.svg" alt="icono menu responsive">
+                </div>
+
+                <div class="derecha">
+                    <img class="dark-mode-boton" src="build/img/dark-mode.svg">
+                    <nav class="navegacion">
+                        <a href="nosotros.html">Nosotros</a>
+                        <a href="anuncios.html">Anuncios</a>
+                        <a href="blog.html">Blog</a>
+                        <a href="contacto.html">Contacto</a>
+                    </nav>
+                </div>
+   
+                
+            </div> <!--.barra-->
+
+            <h1>Venta de Casas y Departamentos  Exclusivos de Lujo</h1>
+        </div>
+    </header>
--- numero mayor/mayor.js
+++
@@ -1,9 +0,0 @@
-var valor1 = parseInt(window.prompt("Ingrese un numero")); 
-var valor2 = parseInt(window.prompt("Ingrese otro numero")); 
-
-if(valor1 > valor2){
-    alert("Valor 1 es mayor");
-}
-else{
-    alert("Valor 2 es mayor");
-}
