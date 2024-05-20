39b549cf76426599a7aee5a9b55fb401526c95fb
yosuad <yosuad@hotmail.com>
2024-05-20 16:51:31 -0500

    bienes_raices_avance

--- includes/app.php
+++
@@ -1,4 +0,0 @@
-<?php
-
-define('TEMPLATES_URL', __DIR__ . '/templates');
-define('FUNCIONES_URL', 'funciones.php');
--- includes/funciones.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-require 'app.php';
-
-function incluirTemplates( string $nombre, bool $inicio = false){        
-    include TEMPLATES_URL . "/{$nombre}.php";
-}
